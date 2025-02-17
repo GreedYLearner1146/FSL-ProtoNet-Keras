a6811a445e85165a999031340986dffb73eb2bda
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-02-18 01:58:56 +0800

    Create Hyperparameters.py

---
+++ Hyperparameters.py
@@ -0,0 +1,13 @@
+# Specify the number of ways (class), and the number of support and query shot.
+# For reference, the query sample per episodic training is n*way*k_shot
+
+############## For 1-shot learning ####################
+n_way = 5   # Number of class revealed per episode
+k_shot = 1  # Number of support shot.
+query_samples_per_class = 5 # Number of query samples per class.
+
+################ For 5-shot learning #####################
+
+n_way = 5   # Number of class revealed per episode
+k_shot = 5  # Number of support shot.
+query_samples_per_class = 25 # Number of query samples per class.
